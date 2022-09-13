b4a14cb94e325d3c12410f64f17b3c808e4add7a
daniel <albertagoya@gmail.com>
2022-09-13 11:02:18 +0300

    managing serializer foreign key

--- cropmonitor/api/serializers.py
+++ cropmonitor/api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from planting.models import *
 class PlantingSerializer(serializers.ModelSerializer):
+    value_chain_name = serializers.ReadOnlyField(
+        source="valuechain.name"
+    )
     class Meta:
         model = PlantingDatePlannerC
-        fields = "__all__"
-        depth = 1
+        # fields = "__all__"
+        # depth = 1
+        fields=["min_maturity_period","max_maturity_period","first_weeding_after_days","second_weeding_after_days",
+                "ferlizer_application_recommendation","min_expected_yield","max_expected_yield",
+                "areas_for_optimal_production","special_attributes","sow_end_date","sow_start_date","refyear",
+                "vc_variety","valuechain","ward","subcounty","county","value_chain_name"
+                ]
