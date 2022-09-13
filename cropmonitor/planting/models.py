68a4f428bc0c44cf23b8435b49131b252810cd75
daniel <albertagoya@gmail.com>
2022-09-13 09:54:04 +0300

    subcounty

--- cropmonitor/planting/models.py
+++ cropmonitor/planting/models.py
@@ -136,7 +136,7 @@ class CropCalendar(models.Model):
   date_created = models.DateTimeField(auto_now_add=True)
   date_updated = models.DateTimeField(auto_now=True)
   def __str__(self):
-        return str(self.name)
+        return str(self.valuechain)
   
 
 
