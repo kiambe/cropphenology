3dbdc24bb23cf85d558c9cdafd087af814d90694
daniel <albertagoya@gmail.com>
2022-09-13 09:22:51 +0300

    subcounty

--- cropmonitor/planting/models.py
+++ cropmonitor/planting/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-length = 200
+length = 2000
 class ValueChain(models.Model):    
   id = models.AutoField(primary_key=True)
   name = models.CharField(max_length=256, null=False) 
