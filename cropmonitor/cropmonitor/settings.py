0a3d2dd3a3afdc894ec4b9940115a978ba5a8536
daniel <albertagoya@gmail.com>
2022-09-13 07:30:33 +0300

    Adding postgres

--- cropmonitor/cropmonitor/settings.py
+++ cropmonitor/cropmonitor/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'cropmonitor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kiambephenology',
+        'USER': 'admin',
+        'PASSWORD': 'D@t@sc13nc3',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
