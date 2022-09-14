58e1dee96911ca4f9603b549e8605152ca02900b
daniel <albertagoya@gmail.com>
2022-09-14 09:22:22 +0300

    adding foreign key to serializer

--- cropmonitor/api/serializers.py
+++ cropmonitor/api/serializers.py
@@ -4,6 +4,12 @@ class PlantingSerializer(serializers.ModelSerializer):
     value_chain_name = serializers.ReadOnlyField(
         source="valuechain.name"
     )
+    county_name = serializers.ReadOnlyField(
+        source='county.name')
+    subcounty_name = serializers.ReadOnlyField(
+        source='subcounty.name')
+    ward_name = serializers.ReadOnlyField(
+        source='ward.name')
     class Meta:
         model = PlantingDatePlannerC
         # fields = "__all__"
@@ -11,5 +17,5 @@ class PlantingSerializer(serializers.ModelSerializer):
         fields=["min_maturity_period","max_maturity_period","first_weeding_after_days","second_weeding_after_days",
                 "ferlizer_application_recommendation","min_expected_yield","max_expected_yield",
                 "areas_for_optimal_production","special_attributes","sow_end_date","sow_start_date","refyear",
-                "vc_variety","valuechain","ward","subcounty","county","value_chain_name"
+                "vc_variety","valuechain","ward","subcounty","county","value_chain_name",'county_name','subcounty_name','ward_name'
                 ]
